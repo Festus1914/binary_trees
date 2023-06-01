411c82b5201cddeab74492d7ad7aa7c29801b954
Festus1914 <oladiplentyfestus@gmail.com>
2023-05-31 23:25:26 -0700

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_treess.h"
+
+/**
+ * binary_tree_delete - This deletes a binary tree
+ * tree - Point to the root node to delete.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if(tree!=NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
